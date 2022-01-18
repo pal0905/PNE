f7ae87a6ade60d5f504219e9f5ad49d13f502501
pal0905 <97714146+pal0905@users.noreply.github.com>
2022-01-18 11:37:59 +0900

    Add files via upload

---
+++ js/78.dd7822ca.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunktsuihen"]=self["webpackChunktsuihen"]||[]).push([[78],{78:function(t,e,n){n.r(e),n.d(e,{startStatusTap:function(){return r}});var o=n(65),s=n(587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const r=()=>{const t=window;t.addEventListener("statusTap",(()=>{(0,o.wj)((()=>{const e=t.innerWidth,n=t.innerHeight,r=document.elementFromPoint(e/2,n/2);if(!r)return;const i=r.closest("ion-content");i&&new Promise((t=>(0,s.c)(i,t))).then((()=>{(0,o.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await i.scrollToTop(300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.dd7822ca.js.map
